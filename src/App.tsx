6778ed389b9a713b3b3a8b02e7943a082d1d3dec
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-09-04 06:16:48 +0300

    create routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 	return (
 		<>
-			<Button apperance='big' onClick={() => {}}>Оформить</Button>
+			<Button apperance="big" onClick={() => {}}>
+        Оформить
+			</Button>
 			<Button onClick={() => {}}>Применить</Button>
 			<Input />
+			<div>
+				<a href="/">Menu</a>
+				<a href="/cart">Cart</a>
+			</div>
+			<Routes>
+				<Route path="/" element={<Menu />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
 		</>
 	);
 }
