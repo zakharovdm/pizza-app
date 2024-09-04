f9eaf1f6ae39ff41d0b188007401812373f2285a
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-09-04 06:33:59 +0300

    change create router on createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
-import { Error } from './pages/Error/Error';
 
 function App() {
 	return (
@@ -14,14 +10,9 @@ function App() {
 			<Button onClick={() => {}}>Применить</Button>
 			<Input />
 			<div>
-				<a href="/">Menu</a>
-				<a href="/cart">Cart</a>
+				<a href="/">Меню</a>
+				<a href="/cart">Корзина</a>
 			</div>
-			<Routes>
-				<Route path="/" element={<Menu />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="*" element={<Error />} />
-			</Routes>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu.tsx';
+import { Cart } from './pages/Cart/Cart.tsx';
+import { Error } from './pages/Error/Error.tsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu />
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*',
+		element: <Error />
+	}
+]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
