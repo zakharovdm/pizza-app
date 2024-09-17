342ae59a5aeff1f71854e688ccf88dc8882f1132
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-09-17 06:28:44 +0300

    add axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,18 +5,16 @@ import SearchInput from '../../components/SearchInput/SearchInput';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
 	const [products, setProducts] = useState<Product[]>([]); 
 
 	const getMenu = async () =>  {
 		try {
-			const res = await fetch(`${PREFIX}/products`);
-			if (!res.ok) {
-				return;
-			}
-			const data = await res.json() as Product[];
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
+
 		} catch(e) {
 			console.error(e);
 			return;
@@ -26,6 +24,7 @@ export function Menu() {
 	useEffect(() => {
 		getMenu();
 	}, []);
+
 
 	return (
 		<>
