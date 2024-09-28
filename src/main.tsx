b5759328e2189895b2882b88402204a22a9cb5bc
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-09-28 06:32:15 +0300

    add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { PREFIX } from './helpers/API.ts';
 import { Login } from './pages/Login/Login.tsx';
 import { Register } from './pages/Register/Register.tsx';
 import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.tsx';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -71,6 +73,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</StrictMode>
 );
