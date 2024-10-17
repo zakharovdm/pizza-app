b0df332bd8f8a07031174dec8b753f659182c3cc
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-10-17 07:38:03 +0300

    add amount calculation

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,10 +8,21 @@ import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import styles from './Cart.module.css';
 
+const DELIVERY_FEE = 169;
+
 export function Cart() {
 	const [cartProducts, setCartProducts] = useState<Product[]>([]);
 
 	const items = useSelector((state: RootState) => state.cart.items);
+	const total = items
+		.map((i) => {
+			const product = cartProducts.find((p) => p.id === i.id);
+			if (!product) {
+				return 0;
+			}
+			return i.count * product.price;
+		})
+		.reduce((acc, i) => (acc += i), 0);
 
 	const getItem = async (id: number) => {
 		const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
@@ -19,7 +30,7 @@ export function Cart() {
 	};
 
 	const loadAllitems = async () => {
-		const res = await Promise.all(items.map(i => getItem(i.id)));
+		const res = await Promise.all(items.map((i) => getItem(i.id)));
 		setCartProducts(res);
 	};
 
@@ -27,16 +38,36 @@ export function Cart() {
 		loadAllitems();
 	}, [items]);
 
-	return <>
-		<Headline className={styles.headline}>
-			Корзина
-		</Headline>
-		{items.map(i => {
-			const product = cartProducts.find(p => p.id === i.id);
-			if (!product) {
-				return;
-			}
-			return <CartItem key={product.id} count={i.count} {...product} />;
-		})}
-	</>;
+	return (
+		<>
+			<Headline className={styles.headline}>Корзина</Headline>
+			{items.map((i) => {
+				const product = cartProducts.find((p) => p.id === i.id);
+				if (!product) {
+					return;
+				}
+				return <CartItem key={product.id} count={i.count} {...product} />;
+			})}
+			<div className={styles.line}>
+				<div className={styles.text}>Итог</div>
+				<div className={styles.price}>
+					{total}&nbsp;<span>₽</span>
+				</div>
+			</div>
+			<hr className={styles.hr} />
+			<div className={styles.line}>
+				<div className={styles.text}>Доставка</div>
+				<div className={styles.price}>
+					{DELIVERY_FEE}&nbsp;<span>₽</span>
+				</div>
+			</div>
+			<hr className={styles.hr} />
+			<div className={styles.line}>
+				<div className={styles.text}>Итог {items.length}</div>
+				<div className={styles.price}>
+					{total + DELIVERY_FEE}&nbsp;<span>₽</span>
+				</div>
+			</div>
+		</>
+	);
 }
